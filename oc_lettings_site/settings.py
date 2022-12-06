c0dff7378166da8ca2c4784fc1beff3375b4f3bc
emualliug1 <98762631+emualliug1@users.noreply.github.com>
2022-12-06 14:33:12 +0100

    refactor

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -5,11 +5,8 @@ import dj_database_url
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
-
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Initialise environment variables
 env = environ.Env(DEBUG=(bool, False))
