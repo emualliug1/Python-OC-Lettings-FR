86e3fbd58ed26834e6abeef14911a61be4f0c5c7
emualliug1 <98762631+emualliug1@users.noreply.github.com>
2022-12-06 15:00:36 +0100

    refactor

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -13,7 +13,7 @@ env = environ.Env(DEBUG=(bool, False))
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 DEBUG = env('DEBUG')
 SECRET_KEY = env('SECRET_KEY', default=get_random_secret_key())
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', f'{env("HEROKU_APP_NAME")}.herokuapp.com']
+ALLOWED_HOSTS = []
 
 # Application definition
 
