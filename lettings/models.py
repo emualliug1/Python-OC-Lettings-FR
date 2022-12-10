0e1b093a989be7812b210ae991c7d1059202eba2
emualliug1 <98762631+emualliug1@users.noreply.github.com>
2022-12-10 16:23:59 +0100

    new test for circle-ci

--- lettings/models.py
+++ lettings/models.py
@@ -3,15 +3,15 @@ from django.core.validators import MaxValueValidator, MinLengthValidator
 
 
 class Address(models.Model):
-    class Meta:
-        verbose_name_plural = 'addresses'
-
     number = models.PositiveIntegerField(validators=[MaxValueValidator(9999)])
     street = models.CharField(max_length=64)
     city = models.CharField(max_length=64)
     state = models.CharField(max_length=2, validators=[MinLengthValidator(2)])
     zip_code = models.PositiveIntegerField(validators=[MaxValueValidator(99999)])
     country_iso_code = models.CharField(max_length=3, validators=[MinLengthValidator(3)])
+
+    class Meta:
+        verbose_name_plural = 'addresses'
 
     def __str__(self):
         return f'{self.number} {self.street}'
