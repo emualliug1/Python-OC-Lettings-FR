c7178f72708fe4566162a78df5052415e8c0d490
emualliug1 <98762631+emualliug1@users.noreply.github.com>
2022-12-10 18:23:38 +0100

    new test for circle-ci

--- lettings/tests.py
+++ lettings/tests.py
@@ -1,22 +1,32 @@
-import pytest
 from django.urls import reverse
+from django.test import TestCase
+from .models import Address, Letting
 
 
-@pytest.mark.django_db
-def test_lettings_index(client):
-    url = reverse('lettings:index')
-    response = client.get(url)
-    html = response.content.decode()
-    expected_content = "<title>Lettings</title>"
-    assert response.status_code == 200
-    assert expected_content in html
+class TestLetting(TestCase):
+    def setUp(self):
+        self.address = Address.objects.create(
+            number=999,
+            street="test",
+            city="testtest",
+            state="test",
+            zip_code=44000,
+            country_iso_code="FRA"
+        )
+        self.letting = Letting.objects.create(title="LettingTest", address=self.address)
 
+    def test_lettings_index(self):
+        url = reverse('lettings:index')
+        response = self.client.get(url)
+        html = response.content.decode()
+        expected_content = "<title>Lettings</title>"
+        assert response.status_code == 200
+        assert expected_content in html
 
-@pytest.mark.django_db
-def test_letting_detail(client):
-    url = reverse('lettings:letting', args=[1])
-    response = client.get(url)
-    html = response.content.decode()
-    expected_content = "<h1>Test Letting</h1>"
-    assert response.status_code == 200
-    assert expected_content in html
+    def test_letting_detail(self):
+        url = reverse('lettings:letting', args=[1])
+        response = self.client.get(url)
+        html = response.content.decode()
+        expected_content = "<h1>LettingTest</h1>"
+        assert response.status_code == 200
+        assert expected_content in html
--- profiles/tests.py
+++ profiles/tests.py
@@ -1,22 +1,30 @@
-import pytest
+from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import Profile
 
 
-@pytest.mark.django_db
-def test_profiles_index(client):
-    url = reverse('profiles:index')
-    response = client.get(url)
-    html = response.content.decode()
-    expected_content = "<title>Profiles</title>"
-    assert response.status_code == 200
-    assert expected_content in html
+class TestProfile(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="TestUser",
+            password="TestPassword",
+            email="test_user@test.com"
+        )
+        self.profile = Profile.objects.create(user=self.user, favorite_city="Nantes")
 
+    def test_profiles_index(self):
+        url = reverse('profiles:index')
+        response = self.client.get(url)
+        html = response.content.decode()
+        expected_content = "<title>Profiles</title>"
+        assert response.status_code == 200
+        assert expected_content in html
 
-@pytest.mark.django_db
-def test_profile_detail(client):
-    url = reverse('profiles:profile', kwargs={'username': '4meRomance'})
-    response = client.get(url)
-    html = response.content.decode()
-    expected_content = "<title>HeadlinesGazer</title>"
-    assert response.status_code == 200
-    assert expected_content in html
+    def test_profile_detail(self):
+        url = reverse('profiles:profile', args=['TestUser'])
+        response = self.client.get(url)
+        html = response.content.decode()
+        expected_content = "<title>TestUser</title>"
+        assert response.status_code == 200
+        assert expected_content in html
